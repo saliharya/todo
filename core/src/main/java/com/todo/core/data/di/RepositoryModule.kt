f6489bc31e68be38d13af1e9877d6ccfc69fc45c
saliharya <saliharyazinc@gmail.com>
2025-07-30 20:48:56 +0700

    Fix: Use TodoRepository interface in Koin module

--- core/src/main/java/com/todo/core/data/di/RepositoryModule.kt
+++ core/src/main/java/com/todo/core/data/di/RepositoryModule.kt
@@ -1,8 +1,9 @@
 package com.todo.core.data.di
 
 import com.todo.core.data.repository.TodoRepositoryImpl
+import com.todo.core.domain.repository.TodoRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single<TodoRepositoryImpl> { TodoRepositoryImpl(get()) }
+    single<TodoRepository> { TodoRepositoryImpl(get()) }
 }
