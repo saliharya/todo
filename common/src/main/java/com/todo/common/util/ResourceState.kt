fc2a6ae70cb4c7d30d98a88fb500327bd6a8d3c5
saliharya <saliharyazinc@gmail.com>
2025-07-30 15:56:39 +0700

    Fix: Update constant import in ResourceState

--- common/src/main/java/com/todo/common/util/ResourceState.kt
+++ common/src/main/java/com/todo/common/util/ResourceState.kt
@@ -1,12 +1,13 @@
 package com.todo.common.util
 
 import com.google.gson.annotations.SerializedName
+import com.todo.common.util.Constant
 
 sealed class ResourceState<T> {
-    data class Loading<T>(val data: T? = null): ResourceState<T>()
+    data class Loading<T>(val data: T? = null) : ResourceState<T>()
     data class Success<T>(val data: T?) : ResourceState<T>()
     data class Error<T>(
-        @SerializedName("message") val message: String? = Constants.SOMETHING_WENT_WRONG,
+        @SerializedName("message") val message: String? = Constant.SOMETHING_WENT_WRONG,
         val data: T? = null,
     ) : ResourceState<T>()
 }
